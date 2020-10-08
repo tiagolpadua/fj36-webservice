490ea981af7f46d61fed94276f2accf4717e2d8e
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-10-07 22:44:38 -0300

    ex9.3

---
+++ src/br/com/caelum/payfast/oauth2/TokenDao.java
@@ -0,0 +1,21 @@
+package br.com.caelum.payfast.oauth2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TokenDao {
+	private List<String> accessTokens = new ArrayList<>();
+
+	public void adicionaAccessToken(String token) {
+		System.out.println("Adicionando token " + token);
+		accessTokens.add(token);
+	}
+
+	public boolean existeAccessToken(String token) {
+		System.out.println("Verificando token " + token);
+		return accessTokens.contains(token);
+	}
+}
